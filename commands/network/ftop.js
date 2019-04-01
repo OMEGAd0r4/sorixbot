0e1189099896b4b0f611e3b382d9a01ecb69ce32
OMEGAd0r4 <45790881+OMEGAd0r4@users.noreply.github.com>
2019-04-01 17:50:52 +0700

    Update ftop.js

--- commands/network/ftop.js
+++ commands/network/ftop.js
@@ -21,7 +21,7 @@ class ftopCommand extends commando.Command {
     message.channel.send({embed: new Discord.RichEmbed()
         .setTitle("**SorixPvP [] Ftop Prizes**")
         .setColor("#4286f4")
-        .setDescription("Week 1: No Payout // TBD \nF Top 1: 50$ PayPal, 45$ Buycraft \nF Top 2: 30$ PayPal, 30$ Buycraft \nF Top 3: 15$ PayPal, 25$ Buycraft \n \nWeek 3: \nF Top 1: 75$ PayPal, 50$ Buycraft \nF Top 2: 50$ PayPal, 30$ Buycraft \nF Top 3: 25$ PayPal, 15$ Buycraft \n \nWeek 4: \nF Top 1: 100$ PayPal, 75$ Buycraft \nF Top 2: 75$ PayPal, 50$ Buycraft \nF Top 3: 50$ PayPal, 25$ Buycraft")
+        .setDescription("Week 1: No Payout // TBD \nWeek 2\nF Top 1: 50$ PayPal, 45$ Buycraft \nF Top 2: 30$ PayPal, 30$ Buycraft \nF Top 3: 15$ PayPal, 25$ Buycraft \n \nWeek 3: \nF Top 1: 75$ PayPal, 50$ Buycraft \nF Top 2: 50$ PayPal, 30$ Buycraft \nF Top 3: 25$ PayPal, 15$ Buycraft \n \nWeek 4: \nF Top 1: 100$ PayPal, 75$ Buycraft \nF Top 2: 75$ PayPal, 50$ Buycraft \nF Top 3: 50$ PayPal, 25$ Buycraft")
         .setTimestamp()
         .setImage("https://cdn.discordapp.com/attachments/543003685676908544/561764845188546590/image0-1.jpg")
         .setFooter("Sorix Bot [] Created by hieu#0843")})
