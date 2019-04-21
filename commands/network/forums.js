0bf9147ecdb0c589faffd5f86808b0e1b3a42b57
OMEGAd0r4 <45790881+OMEGAd0r4@users.noreply.github.com>
2019-04-21 14:09:28 +0700

    Update forums.js

--- commands/network/forums.js
+++ commands/network/forums.js
@@ -21,10 +21,10 @@ class forumsCommand extends commando.Command {
     message.channel.send({embed: new Discord.RichEmbed()
         .setTitle("**SorixPvP [] Forums**")
         .setColor("#4286f4")
-        .setDescription("The forums page of the **SorixPvP Network** is **http://sorixpvp.com/**")
+        .setDescription("The forums page of the **Trexic Network** is **http://trexic.org/**")
         .setTimestamp()
-        .setImage("https://cdn.discordapp.com/attachments/543003685676908544/561764845188546590/image0-1.jpg")
-        .setFooter("Sorix Bot [] Created by hieu#0843")})
+        .setImage("https://cdn.discordapp.com/attachments/543003685676908544/564930926023278602/Logo-1.png")
+        .setFooter("Trexic Bot [] Created by hieu#0843")})
   }
 }
 
